872c77111c22a6fc6003d8efc8fd2ead3b6df664
ahara <haras.adam@gmail.com>
2017-05-23 22:02:26 +0100

    Add ExtraTree model

--- xgb.py
+++ xgb.py
@@ -52,7 +52,7 @@ def get_train_validation_sets(testset=False, reverse=False):
 
 
 if __name__ == '__main__':
-    submission_mode = False
+    submission_mode = True
     x_train, x_valid, y_train, y_valid = get_train_validation_sets(reverse=False)
     #x_train, x_valid, y_train, y_valid, x_valid_reverse = get_train_validation_sets(reverse=True)
 
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     params = {}
     params['objective'] = 'binary:logistic'
     params['eval_metric'] = 'logloss'
-    params['eta'] = 0.05  # 0.0125
+    params['eta'] = 0.0125  # 0.0125 - 2400 iter, 0.05 - 600 iter
     params['max_depth'] = 16  # 11
     params['subsample'] = 0.8
     params['max_delta_step'] = 1
@@ -74,16 +74,16 @@ if __name__ == '__main__':
 
     d_train = xgb.DMatrix(x_train, label=y_train, missing=MISSING)
     d_valid = xgb.DMatrix(x_valid, label=y_valid, missing=MISSING)
-    d_valid_reverse = xgb.DMatrix(x_valid_reverse, label=y_valid, missing=MISSING)
+    #d_valid_reverse = xgb.DMatrix(x_valid_reverse, label=y_valid, missing=MISSING)
 
     watchlist = [(d_train, 'train'), (d_valid, 'valid')]
 
     print 'Start training'
 
-    bst = xgb.train(params, d_train, 600, watchlist, early_stopping_rounds=50, verbose_eval=10)  # 2400
+    bst = xgb.train(params, d_train, 2400, watchlist, early_stopping_rounds=50, verbose_eval=10)  # 2400
     del d_train, d_valid
 
-    clf = ExtraTreesClassifier(n_estimators=1200, criterion='entropy', random_state=999,
+    clf = ExtraTreesClassifier(n_estimators=600, criterion='entropy', random_state=999,
                                class_weight=class_weights)
     x_train2 = x_train.replace([np.inf, -np.inf], np.nan).fillna(MISSING)
     x_valid2 = x_valid.replace([np.inf, -np.inf], np.nan).fillna(MISSING)
@@ -94,12 +94,19 @@ if __name__ == '__main__':
     xgb_preds = bst.predict(xgb.DMatrix(x_valid.fillna(MISSING), missing=MISSING))
     print 'XGB and Extra Trees:', log_loss(y_valid, (0.1 * et_preds[:, 1] + 0.9 * xgb_preds))
 
-    import pdb
-    pdb.set_trace()
+    #import pdb
+    #pdb.set_trace()
+
+    del x_train, x_valid, x_train2, x_valid2, et_preds, xgb_preds
 
     x_test, test_id = get_train_validation_sets(testset=True)
+    x_test.replace([np.inf, -np.inf], np.nan, inplace=True)
+    x_test.fillna(MISSING, inplace=True)
 
-    preds = bst.predict(xgb.DMatrix(x_test.fillna(MISSING), missing=MISSING))
+    et_preds = clf.predict_proba(x_test)
+    del clf
+    xgb_preds = bst.predict(xgb.DMatrix(x_test, missing=MISSING))
+    preds = (0.1 * et_preds[:, 1] + 0.9 * xgb_preds)  # Ensemble XGBoost and ExtraTrees
 
     print("Writing output...")
     sub = pd.DataFrame()
