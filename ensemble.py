f8c695b13f2bf4b50cee61a7397e1467d769d706
ahara <haras.adam@gmail.com>
2017-06-06 19:44:17 +0100

    Add forum model XGB_leaky to ensemble

--- ensemble.py
+++ ensemble.py
@@ -1,18 +1,10 @@
 import pandas as pd
-from sklearn.metrics import log_loss
-
-from xgb import get_train_validation_sets
-
-
-#_, _, _, y_val = get_train_validation_sets()
-#xgb_val = pd.read_csv('xgb_valid.csv');
-#sgd_fm_val = pd.read_csv('../kaggle_quora_benchmark/valid.csv')
-#print log_loss(y_val, xgb_val['is_duplicate'].values * 0.95 + sgd_fm_val['is_duplicate'].values * 0.05)
 
 xgb_preds = pd.read_csv('xgb_preds.csv')
+xgb_leaky = pd.read_csv('XGB_leaky.csv')
 sgd_fm = pd.read_csv('../kaggle_quora_benchmark/sgd_fm.csv')
 
 sub = pd.DataFrame()
 sub['test_id'] = xgb_preds['test_id']
-sub['is_duplicate'] = xgb_preds['is_duplicate'] * 0.9 + sgd_fm['is_duplicate'] * 0.1
+sub['is_duplicate'] = xgb_preds['is_duplicate'] * 0.5 + xgb_leaky['is_duplicate'] * 0.4 + sgd_fm['is_duplicate'] * 0.1
 sub.to_csv('ensemble_preds.csv', index=False)
