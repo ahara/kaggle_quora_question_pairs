50eff3cfb097fb1447d0761d413e8aedae26a31d
ahara <haras.adam@gmail.com>
2017-06-06 19:57:28 +0100

    Remove xgb_leaky Add xgb with oversampling Change weights

--- ensemble.py
+++ ensemble.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 xgb_preds = pd.read_csv('xgb_preds.csv')
-xgb_leaky = pd.read_csv('XGB_leaky.csv')
+xgb_over = pd.read_csv('xgb_preds_oversample.csv')
 sgd_fm = pd.read_csv('../kaggle_quora_benchmark/sgd_fm.csv')
 
 sub = pd.DataFrame()
 sub['test_id'] = xgb_preds['test_id']
-sub['is_duplicate'] = xgb_preds['is_duplicate'] * 0.5 + xgb_leaky['is_duplicate'] * 0.4 + sgd_fm['is_duplicate'] * 0.1
+sub['is_duplicate'] = xgb_preds['is_duplicate'] * 0.7 + xgb_over['is_duplicate'] * 0.15 + sgd_fm['is_duplicate'] * 0.15
 sub.to_csv('ensemble_preds.csv', index=False)
